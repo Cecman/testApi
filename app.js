828750f528cc5ffa594262cce51d2dba8b011acb
Cecman <jane.trajkoski@gmail.com>
2021-03-30 16:08:43 +0200

    updateOne added

--- app.js
+++ app.js
@@ -66,15 +66,31 @@ app
 
 //specific article route
 
-app.route("/articles/:articleTitle").get((req, res) => {
-  Article.findOne({ title: req.params.articleTitle }, (err, foundArticle) => {
-    if (foundArticle) {
-      res.send(foundArticle);
-    } else {
-      res.send("No articles matching the parameters");
-    }
+app
+  .route("/articles/:articleTitle")
+  .get((req, res) => {
+    Article.findOne({ title: req.params.articleTitle }, (err, foundArticle) => {
+      if (foundArticle) {
+        res.send(foundArticle);
+      } else {
+        res.send("No articles matching the parameters");
+      }
+    });
+  })
+  .put((req, res) => {
+    Article.update(
+      { title: req.params.articleTitle },
+      { title: req.body.title, content: req.body.content },
+      { overwrite: true },
+      (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.send("Succesfully updated the article");
+        }
+      }
+    );
   });
-});
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
